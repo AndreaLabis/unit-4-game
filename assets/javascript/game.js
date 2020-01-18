8afd3fca555053b2418d3dc9512a89b91e433f66
Andrea Labis <sweetlila111282@aol.com>
2020-01-17 21:45:37 -0500

    adding additonal fucntionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,80 @@
-
-// Here we establish the "targetNumber" (set to 50 in this example).
-
-$(document).ready(function() {
-
+$(function() {
   var targetNumber;
-  var Score = 0;
+  var wins = 0;
+  var losses = 0;
+  var userScore;
 
-
-
-  $("#number-to-guess").text(targetNumber); //this will be between 19 - 120.
-
-
-  let wins = 0
-  let losses = 0;
-  let counter = 0;
-  let incrementOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-  for (var i = 0; i < incrementOptions.length; i++) {
-      console.log(incrementOptions[i]);
-  }  
-
-    var crystalVal = incrementOptions[Math.round(Math.random() * incrementOptions.length)];
-
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-images");
-    imageCrystal.attr("src", url= "/assets/images/crystal1.png", "/assets/images/crystal2.png", "/assets/images/crystal3.png", "/assets/images/crystal4.png");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-
-
-    $(".crystal-image").on("click", function () {
-      counter += crystalVal;
-      $("#crystals").append(imageCrystal);
-    
-   
-     alert('Your new score is: ' + counter);
-
-    if (counter === targetNumber) {
-
-      alert("You win!");
+  function reset() {
+    for(var i = 0; i < crystals.length; i++) {
+        crystals[i].setAttribute("score", Math.floor(Math.random() * 11 + 1))
     }
+    targetNumber = Math.floor(Math.random() * 101 + 19);
+    userScore = 0;
+    $("#user-Score").text(userScore);
+    $("#target-number").text(targetNumber);
+  }
 
-    else if (counter >= targetNumber) {
-
-      alert("You lose!");
-
+  function initialize() {
+    crystals = document.getElementsByTagName("img")
+    for(var i = 0; i < crystals.length; i++) {
+        crystals[i].setAttribute("score", Math.floor(Math.random() * 11 + 1))
+        crystals[i].addEventListener("click", (args) => {
+            value = Math.round(args.target.getAttribute("score"))
+            userScore = userScore + value;
+            $("#user-Score").text(userScore);
+            console.log(userScore);
+            logic();
+        })
+    }
+    targetNumber = Math.floor(Math.random() * 101 + 19);
+    $("#target-number").text(targetNumber);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#user-score").text(userScore);
+  }
+  function logic() {
+    if (userScore === targetNumber) {
+        alert("You Win!");
+        reset();
+        wins++;
+        $("#wins").text(wins);
+    }
+    else if (userScore > targetNumber) {
+        alert("You lose!");
+        reset();
+        losses++;
+        $("#losses").text(losses);
     }
   }
 
-  
+  initialize();
 
+  $("#green").on("click", function () {
+  userScore += num1;
+  $("#user-score").text(userScore);
+  console.log(userScore);
+  logic();
+  })
 
+  $("#red").on("click", function () {
+  userScore += num2;
+  $("#user-score").text(userScore);
+  console.log(userScore);
+  logic();
+  })
 
+  $("#blue").on("click", function () {
+  userScore += num3;
+  $("#user-score").text(userScore);
+  console.log(userScore);
+  logic();
+  })
 
-  });
+  $("#orange").on("click", function () {
+  userScore += num4;
+  $("#user-score").text(userScore);
+  console.log(userScore);
+  logic();
+  })
+
 }); 
